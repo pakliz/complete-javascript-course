9689b8059bfa36d889073362898e9b5149c71e82
Chonghwa Pak <tdog-macbook@Chonghwas-MacBook-Pro.local>
2019-07-06 21:31:02 -0400

    first commit

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,50 @@
+//console.log('Hello World!!!');
+
+/*
+let firstName = 'John';
+console.log(firstName);
+let lastName = 'Smith';
+let age = 28;
+let fullAge = true;
+console.log(fullAge);
+let job;
+console.log(job);
+job = "Teacher";
+console.log(job);
+
+
+let firstName = 'John';
+let age = 28;
+
+console.log(firstName + " " + age); //type coercion
+
+let job, isMarried;
+job = 'teacher';
+isMarried = false;
+console.log(firstName + ' is a ' + age + ' ' + job + '. Is he married? ' + isMarried);
+
+//variable mutation
+age = 'twenty eight';
+job = 'driver';
+
+//alert(firstName + ' is a ' + age + ' ' + job + '. Is he married? ' + isMarried);
+
+//prompt can prompt the users last name
+let lastName = prompt('what is his last name?');
+console.log(firstName + ' ' + lastName);
+
+*/
+
+//Basic Operators, typeof
+console.log(typeof "john");
+console.log(typeof false);
+console.log(typeof 28);
+
+//operator precedence
+let now = 2018;
+let yearJohn = 1989;
+let fullAge = 18; 
+
+let isFullAge = now - yearJohn >= fullAge;
+console.log(isFullAge);
+
